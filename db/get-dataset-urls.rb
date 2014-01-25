a242887790b374f012107b70a765ec4bc396e1b8
Brad Ackerman <brad@facefault.org>
2014-01-25 01:24:52 -0500

    Only retrieve datasets we actually want. Add start of administration guide.

--- db/get-dataset-urls.rb
+++ db/get-dataset-urls.rb
@@ -4,14 +4,32 @@ require 'nokogiri'
 require 'open-uri'
 require 'uri'
 
+# Location of ULS dump files
+WEEKLY_DUMP_URL =
+  'http://wireless.fcc.gov/uls/index.htm?job=transaction&page=weekly'
+# Filename to read database selections from
+CONFIG_FILENAME = "include-datasets.conf"
+
+# Open our configuration file and determine which datasets to download.
+config_file = File.join(File.dirname(__FILE__), CONFIG_FILENAME)
+datasets = []
+File.open config_file do |file|
+  file.each_line do |line|
+    if line.match(/^([^#]\S+)/)
+      datasets.push $1
+    end
+  end
+end
+
 zips = []
-doc = Nokogiri::HTML(
-  open 'http://wireless.fcc.gov/uls/index.htm?job=transaction&page=weekly')
+doc = Nokogiri::HTML(open WEEKLY_DUMP_URL)
 doc.xpath('//a').each do |link|
   href = link.attributes["href"]
   if href
-    if href.value.match /l_([a-z])+\.zip$/i
-      zips.push href.value
+    if href.value.match(/l_([a-z]+)\.zip$/i)
+      if datasets.include? $1
+        zips.push href.value
+      end
     end
   end
 end
